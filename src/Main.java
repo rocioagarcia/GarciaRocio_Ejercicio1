3000078c30767ef0b059a84ceed9e0aa5e6db789
rocio <rocio@192.168.1.11>
2021-04-30 15:56:48 -0300

    test 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		//Comentario de prueba
+
 		Scanner scan = new Scanner(System.in);
 		
 		String nombre; 
